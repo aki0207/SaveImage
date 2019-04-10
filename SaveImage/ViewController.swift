5ca26eca212545839a70d1d02d4d3089ca715a88
Akihiro Nagami <nagami@kikyos.co.jp>
2019-04-10 14:02:03 +0900

    Initial Commit

---
+++ SaveImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveImage
+//
+//  Created by 永見彰宏 on 2019/04/10.
+//  Copyright © 2019 永見彰宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
